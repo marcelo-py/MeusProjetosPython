33ed60b47bf1a7a1b9fdc82f5397b4da525a5501
marcelo-py <79275219+marcelo-py@users.noreply.github.com>
2021-03-29 23:29:26 -0300

    Update car_simulator.py

--- simulador-de-carro-usando-poo/car_simulator.py
+++ simulador-de-carro-usando-poo/car_simulator.py
@@ -8,8 +8,6 @@ class Carro:
         self.carro_andar = None
         self.tocar_musica = None
         self.fechar_carro = None
-
-
     def abrir(self):
         if self.abrir_porta:
             print('As portas já estão abertas! ')
@@ -17,30 +15,29 @@ class Carro:
         print('Portas abertas!')
         self.abrir_porta = True
         self.fechar_carro = False
-
-
     def fechar(self):
         if self.fechar_carro:
             print('O carrro já está com as portas fechadas!')
             return
-
         print('O carro está com as portas fechadas')
         self.fechar_carro = True
         self.abrir_porta = False
 
 
     def entrar(self):
-        if not self.abrir_porta:
-            print('Para entrar você precisa abrir a porta primeiro')
+        if self.entrar_carro:
+            print('Você ja está dentro do carro!')
             return
 
         print('Você entrou no carro e está dentro dele!')
         self.entrar_carro = True
 
-
     def ligar(self):
         if not self.entrar_carro:
             print('A porta não está aberta ou você não está dentro do carro para liga-lo')
+            return
+        if self.ligar_carro:
+            print('O carro já está ligado!')
             return
         if not self.entrar_carro:
             print('Você precisa está dentro do carro para liga-lo')
@@ -55,7 +52,6 @@ class Carro:
         if not self.ligar_carro:
             print('O carro precisa está ligado para andar')
             return
-
 
         print('O carro está andando')
         self.carro_andar = True
@@ -80,9 +76,7 @@ class Carro:
             print('Erro ao tocar música')
         finally:
             print('A música parou de tocar!')
-    
 
-#Programa principal
 c = Carro()
 while True:
     print('\033[1;35m='*38)
@@ -97,10 +91,8 @@ while True:
         6 - Tocar música
     (digite 000 para sair )\033[1;34m=>>\033[m'''))
         print('\033[1;35m~\033[m'*38)
-
     except ValueError:
         print('\033[1;33mErro!\033[m Por favor digite um número correto!')
-        
     else:
         if op == 1:
             c.abrir()
@@ -114,7 +106,6 @@ while True:
             c.andar()
         elif op == 6:
             c.tocarmusica('DEAF KEV - Invincible[NCS].mp3')  # Coloque a música na pasta junto com o programa e coloque o nome dela entre aspas alterando a musica atual, ex: 'musica.mp3'
-            continue
         elif op > 5 or op != 000:
             print('\033[1;31mErro!\033[m Digite uma opção válida!')
         if op == 000:
