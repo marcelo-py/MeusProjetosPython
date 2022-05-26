de1f7bfe977ba677ca76a37f51761d8466cd7c4a
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-05-25 22:25:53 -0300

    crash supostamente resolvido

--- site_gerador_cpf/cpf_gerador/views.py
+++ site_gerador_cpf/cpf_gerador/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .func_cpf_gerador import gerar_cpf
 
 def index(request):
-    if request.method == 'POST':
-        gera_quant = request.POST.get('quant_cpfs')
-        gerados = gerar_cpf(quant=gera_quant)
-
+    if request.method != 'POST':
         return render(request, 'index.html', {
-            'gerados': gerados
+            'gerados': [],
         })
-    return render(request, 'index.html',)
+
+
+    gera_quant = request.POST.get('quant_cpfs')
+    postar_no_arq = bool(request.POST.get('criar_arq'))
+    postar = False
+    print(postar_no_arq, '<<<<<<<<<<<<<<<')
+    gerados = gerar_cpf(quant=gera_quant)
+
+    return render(request, 'index.html', {
+        'gerados': gerados
+    })
+    
 
